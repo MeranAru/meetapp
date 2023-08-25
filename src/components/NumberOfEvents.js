b93f686f69c3718452583c18434b266ffb938b14
Meran Arumugam <meran.arumugam@gmail.com>
2023-08-24 20:27:35 -0400

    added Tests and components

---
+++ src/components/NumberOfEvents.js
@@ -0,0 +1,12 @@
+const NumberOfEvents = () => {
+    return (
+        <div id="number-of-events">
+            <input 
+                type="text" 
+                defaultValue="32">
+            </input>
+        </div>
+    );
+};
+
+export default NumberOfEvents;

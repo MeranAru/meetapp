ff3007c4c0e20070c3804087a06bdfc9ef1811bb
Meran Arumugam <meran.arumugam@gmail.com>
2023-09-08 23:57:57 -0400

    Update api.js
    updated code to use offline

--- src/api.js
+++ src/api.js
@@ -59,6 +59,11 @@ export const getEvents = async () => {
         return mockData;
     }
     
+    if (!navigator.onLine) {
+        const events = localStorage.getItem("lastEvents");
+        return events?JSON.parse(events):[];
+    }
+
     const token = await getAccessToken();
 
     if (token) {
